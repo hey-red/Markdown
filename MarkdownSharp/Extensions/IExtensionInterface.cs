6bfba157c0477831f702da74923212591e6f91c2
Kiririn <ritsufag@gmail.com>
2015-07-03 23:15:54 +0300

    Add interface for extensions

---
+++ MarkdownSharp/Extensions/IExtensionInterface.cs
@@ -0,0 +1,19 @@
+﻿/**
+ * This file is part of the MarkdownSharp package
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+
+namespace MarkdownSharp.Extensions
+{
+    public interface IExtensionInterface
+    {
+        /// <summary>
+        /// Replace inline element
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string Transform(string text);
+    }
+}
