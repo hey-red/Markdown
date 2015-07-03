ac0d7854dcfdc47d16f723c926c722ece7b5381b
Kiririn <ritsufag@gmail.com>
2015-07-04 02:28:42 +0300

    Added myanimelist.net extension for creating short links to user profiles

---
+++ MarkdownSharp/Extensions/Mal/Profile.cs
@@ -0,0 +1,42 @@
+﻿/**
+ * This file is part of the MarkdownSharp package
+ * For the full copyright and license information,
+ * view the LICENSE file that was distributed with this source code.
+ */
+
+using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+
+namespace MarkdownSharp.Extensions.Mal
+{
+    /// <summary>
+    /// Create short link for http://myanimelist.net
+    /// ex: http://myanimelist.net/profile/ritsufag => mal://ritsufag
+    /// </summary>
+    public class Profile : IExtensionInterface
+    {
+        private static Regex _malArticles = new Regex(@"
+                    (?:http\:\/\/)
+                    (?:www\.)?
+                    myanimelist\.net\/profile\/
+                    ([\w-]{2,16})", RegexOptions.Multiline | RegexOptions.IgnorePatternWhitespace);
+
+
+        public string Transform(string text)
+        {
+            return _malArticles.Replace(text, new MatchEvaluator(ProfileEvaluator));
+        }
+
+
+        private string ProfileEvaluator(Match match)
+        {
+            string userName = match.Groups[1].Value;
+            return String.Format(
+                "[mal://{0}](http://myanimelist.net/profile/{1})",
+                userName, userName
+            );
+        }
+    }
+}
