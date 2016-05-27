0509fccf02dfd8f93107c4b421cbf0a3b7199822
Kiririn <ritsufag@gmail.com>
2016-05-27 12:26:26 +0300

    Update assembly info

--- MarkdownSharp/Properties/AssemblyInfo.cs
+++ MarkdownSharp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Eureka")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.14.4.0")]
+[assembly: AssemblyFileVersion("1.14.4.0")]
